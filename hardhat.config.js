783d5a065b1ceb876d02113b37e601f972304555
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-06-30 00:13:37 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,27 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-etherscan');
-require('@nomiclabs/hardhat-waffle');
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
 
 // Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = 'KEY';
+// a new App in its dashboard, and replace "KEY" with its key in the ".env" file.
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
-// Replace this private key with your Ropsten account private key
+// Replace this private key with your Ropsten account private key in the ".env" file.
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = 'KEY';
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
+const HARDHAT_MAINNET_FORK_KEY = process.env.HARDHAT_MAINNET_FORK_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 
-task('accounts', 'Prints the list of accounts', async () => {
+task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
   // eslint-disable-next-line no-restricted-syntax
@@ -34,20 +37,20 @@ task('accounts', 'Prints the list of accounts', async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.4',
+  solidity: "0.8.4",
   paths: {
-    artifacts: './src/artifacts',
+    artifacts: "./src/artifacts",
   },
   networks: {
+    hardhat: {
+      forking: {
+        url: 'https://eth-mainnet.alchemyapi.io/v2/${HARDHAT_MAINNET_FORK_KEY}',
+        blockNumber: 12610259,
+      },
+    },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
-    },
-    hardhat: {
-      forking: {
-        url: 'https://eth-mainnet.alchemyapi.io/v2/KEY',
-        blockNumber: 12610259,
-      },
     },
   },
 };
