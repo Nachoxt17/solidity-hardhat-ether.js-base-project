96b6bb623765a6ce633d28d8247ea8c0afd06674
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2022-01-26 01:18:45 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,21 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer"); //+-By running the Command "npx hardhat size-contracts" we can know the Size of our S.Contracts.
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard in whatever E.V.M. Compatible Blockcahin
 // do you need, and replace "KEY" with its key in the ".env" file.
 const ETH_ROPSTEN_ALCHEMY_API_KEY = process.env.ETH_ROPSTEN_ALCHEMY_API_KEY;
 const ETH_MAINNET_ALCHEMY_API_KEY = process.env.ETH_MAINNET_ALCHEMY_API_KEY;
+
+const POLYGON_MUMBAI_ALCHEMY_API_KEY =
+  process.env.POLYGON_MUMBAI_ALCHEMY_API_KEY;
+const POLYGON_MAINNET_ALCHEMY_API_KEY =
+  process.env.POLYGON_MAINNET_ALCHEMY_API_KEY;
 
 // Replace this private key with your E.V.M. Compatible Blockcahin account private key in the ".env" file.
 // To export your private key from Metamask, open Metamask and
@@ -18,6 +25,8 @@ const ETH_MAINNET_ALCHEMY_API_KEY = process.env.ETH_MAINNET_ALCHEMY_API_KEY;
 const HARDHAT_MAINNET_FORK_KEY = process.env.HARDHAT_MAINNET_FORK_KEY;
 const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
 const ETH_MAINNET_PRIVATE_KEY = process.env.ETH_MAINNET_PRIVATE_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+const POLYGON_MAINNET_PRIVATE_KEY = process.env.POLYGON_MAINNET_PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -38,7 +47,21 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  contractSizer: {
+    alphaSort: false,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+  },
   paths: {
     artifacts: "./src/artifacts",
   },
@@ -49,8 +72,7 @@ module.exports = {
         url: `https://eth-mainnet.alchemyapi.io/v2/${HARDHAT_MAINNET_FORK_KEY}`,
         blockNumber: 12610259,
       },*/
-      chainId: 1337,
-    } /**,
+    } /**
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ETH_ROPSTEN_ALCHEMY_API_KEY}`,
       accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
@@ -59,11 +81,14 @@ module.exports = {
       url: `https://eth-mainnet.alchemyapi.io/v2/${ETH_MAINNET_ALCHEMY_API_KEY}`,
       accounts: [`0x${ETH_MAINNET_PRIVATE_KEY}`],
     },
-  },
-  etherscan: {
-      // Your API key for Etherscan
-      // Obtain one at https://etherscan.io/
-      apiKey: process.env.ETHERSCAN_API_KEY,
-  },*/,
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${POLYGON_MUMBAI_ALCHEMY_API_KEY}`,
+      accounts: [`0x${MUMBAI_PRIVATE_KEY}`],
+    },
+    matic: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${POLYGON_MAINNET_ALCHEMY_API_KEY}`,
+      accounts: [`0x${POLYGON_MAINNET_PRIVATE_KEY}`],
+    },
+  }*/,
   },
 };
