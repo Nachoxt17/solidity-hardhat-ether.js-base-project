97203a95030d7d9ddfc0ffda1f608c11e8b1d7eb
Nachoxt17 <ignacioceaglio@gmail.com>
2022-10-24 21:10:28 +0200

    Update BaseSmartContract.test.js

--- test/BaseSmartContract.test.js
+++ test/BaseSmartContract.test.js
@@ -1,25 +1,52 @@
-const { expect } = require("chai");
-const { ethers, network } = require("hardhat");
 
-var chai = require("chai");
-const BN = require("bn.js");
-chai.use(require("chai-bn")(BN));
-
-function tokens(n) {
-  return web3.utils.toWei(n, "ether");
-}
-
-contract("BaseSmartContract", ([deployer, investor]) => {
-  let baseSmartContract;
-
-  before(async () => {
-    // Test Something.
-  });
 
   describe("BaseSmartContract deployment", async () => {
     it("contract has a name", async () => {
       const name = await baseSmartContract.name();
       assert.equal(name, "SampleSmartContract");
     });
+  });
+});
+
+import { assert } from "chai";
+import { ethers, network } from "hardhat";
+import { envConfig } from "../../config/envs";
+const { DAI, DAI_WHALE, USDC, USDC_WHALE, USDT } = require("./ParametersConfig.js");
+let daiToken: any, usdcToken: any, usdtToken: any, baseSmartContract: any, admin: any, user1: any;
+
+describe("BaseSmartContract deployment", function () {
+  const DECIMALS = 6;//-(Decimals for USDC and USDT. DAI has 18 Decimals).
+
+  before(async () => {
+    const url = `https://eth-mainnet.alchemyapi.io/v2/${envConfig.crypto.ALCHEMY_KEY}`;
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: url,
+            blockNumber: /**14390000*/15269796,
+          },
+        },
+      ],
+    });
+
+    admin = await ethers.getImpersonatedSigner(USDC_WHALE);
+    user1 = await ethers.getImpersonatedSigner(DAI_WHALE);
+
+    daiToken = await ethers.getContractAt("IERC20", DAI, admin);
+    usdcToken = await ethers.getContractAt("IERC20", USDC, admin);
+    usdtToken = await ethers.getContractAt("IERC20", USDT, admin);
+
+    let BaseSmartContract = await ethers.getContractFactory("BaseSmartContract");
+    baseSmartContract = await BaseSmartContract.deploy();
+  });
+
+  it("Contract has a Name", async () => {
+    let name = await baseSmartContract.name();
+    
+    assert.equal(name, "SampleSmartContract", "It doesn't shows Name");
+    
+    console.log(`Smart Contract Name: {name}`);
   });
 });
